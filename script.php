3b53bba2d0b6bc49c24f01ab8541ee2d31d30ea4
Francesco Levanti <francesco@levanti.net>
2018-01-01 14:08:36 +0000

    wip

--- script.php
+++ script.php
@@ -5,3 +5,6 @@
  * Date: 01/01/2018
  * Time: 13:55
  */
+
+include(__DIR__ . '/vendor/autoload.php');
+$a = new flevanti\PhpQueueSystem\PhpQueueSystem();
--- src/PhpQueueSystem/PhpQueueSystem.php
+++ src/PhpQueueSystem/PhpQueueSystem.php
@@ -9,7 +9,29 @@
 namespace flevanti\PhpQueueSystem;
 
 
-class PhpQueueSystem
-{
+class PhpQueueSystem {
+
+  protected $verbose = FALSE;
+
+  function __construct($verbose = NULL) {
+    if (is_null($verbose)) {
+      $this->verbose = PHP_SAPI == 'cli';
+    }
+    elseif ($verbose === TRUE) {
+      $this->verbose = TRUE;
+    }
+    else {
+      $this->verbose = FALSE;
+    }
+
+    $this->e("WELCOME TO PHP QUEUE SYSTEM");
+  }
+
+  protected function e($txt, $nl = PHP_EOL) {
+    if (!$this->verbose) {
+      return;
+    }
+    echo date("d-m-Y H:i:s   ") . $txt . $nl;
+  }
 
 }
