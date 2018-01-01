db5ad58508caec85ed5891aeec4a52c83a35216d
Francesco Levanti <francesco@levanti.net>
2018-01-01 14:08:29 +0000

    wip

---
+++ script.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: francescolevanti
+ * Date: 01/01/2018
+ * Time: 13:55
+ */
---
+++ src/PhpQueueSystem/PhpQueueSystem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: francescolevanti
+ * Date: 01/01/2018
+ * Time: 13:57
+ */
+
+namespace flevanti\PhpQueueSystem;
+
+
+class PhpQueueSystem
+{
+
+}
